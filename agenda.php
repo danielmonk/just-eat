109b2b7e5cda09b0853e5d273b10eb8d0ea03b9a
Hayley Duffin <hduffin1@hotmail.com>
2021-01-28 10:32:09 +0000

    amend gitignore

--- agenda.php
+++ agenda.php
@@ -2,7 +2,7 @@
 
 <?php get_header(); ?>
 
-<div class="banner">
+<div class="banner-two">
     <div class="container">
         <div class="heading-container">
             <div class="heading-wrapper">
@@ -23,88 +23,30 @@
         <div class="agenda-repeater-wrapper">
             <div class="flex">
 
-                <!-- 10:30 morning block -->
-
                 <div class="agenda-section">
-                    <div class="flex">
-                        <img src="<?php the_field('1030_icon');?>" class="icon">
-                        <h3><?php the_field('1030_time');?></h3> 
-                    </div>
-                    <h3><?php the_field('1030_intro');?></h3>
-
-                    <button class="accordion">Find out more about speakers</button>
-                    <div class="panel">
-                        <?php if( have_rows('1030_speakers')): ?>
-                            <?php while(have_rows('1030_speakers')): the_row(); ?>
-
-                                <div class="speaker-details">
-                                    <div class="flex">
-                                        <img src="<?php the_sub_field('speaker_image');?>" class="speaker-img">
-                                        <h3><?php the_sub_field('speaker_name_position');?></h3> 
-                                    </div>
-                                        <?php the_sub_field('speaker_info');?>
-                                    
+                    <?php if( have_rows('agenda_repeater') ): ?>
+                        <?php while(have_rows('agenda_repeater')): the_row(); ?>
+                            
+                            <div class="agenda-content-wrapper">
+                                <div class="flex">
+                                    <img src="<?php the_sub_field('icon');?>" class="icon">
+                                    <h3><?php the_sub_field('time');?></h3>
                                 </div>
+                                <h3><?php the_sub_field('title');?></h3>
 
-                            <?php endwhile; ?>
-                        <?php endif; ?>
-                    </div>
-                </div>
-
-                <!-- 10:45 morning block -->
-
-                <div class="agenda-section">
-                    <div class="flex">
-                        <img src="<?php the_field('1045_icon');?>" class="icon">
-                        <h3><?php the_field('1045_time');?></h3> 
-                    </div>
-                    <h3><?php the_field('1045_intro');?></h3>
-
-                    <button class="accordion">Find out more about speakers</button>
-                    <div class="panel">
-                        <?php if( have_rows('1045_speakers')): ?>
-                            <?php while(have_rows('1045_speakers')): the_row(); ?>
-
-                                <div class="speaker-details">
-                                    <div class="flex">
-                                        <img src="<?php the_sub_field('speaker_image');?>" class="speaker-img">
-                                        <h3><?php the_sub_field('speaker_name_position');?></h3> 
-                                    </div>
-                                        <?php the_sub_field('speaker_info');?>
-                                    
+                                <button class="accordion"><?php the_sub_field('speaker_drop');?></button>
+                                <div class="panel">
+                                    <?php if( have_rows('speaker_repeater') ): ?>
+                                        <?php while(have_rows('speaker_repeater')): the_row(); ?>
+                                            <img src="<?php the_sub_field('image');?>">
+                                            <h3><?php the_sub_field('name');?></h3>
+                                            <?php the_sub_field('content');?>
+                                        <?php endwhile; ?>
+                                    <?php endif; ?>
                                 </div>
-
-                            <?php endwhile; ?>
-                        <?php endif; ?>
-                    </div>
-                </div>
-
-                <!-- 10:45 morning block -->
-
-                <div class="agenda-section">
-                    <div class="flex">
-                        <img src="<?php the_field('1130_icon');?>" class="icon">
-                        <h3><?php the_field('1130_time');?></h3> 
-                    </div>
-                    <h3><?php the_field('1130_intro');?></h3>
-
-                    <button class="accordion">Find out more about speakers</button>
-                    <div class="panel">
-                        <?php if( have_rows('1130_speakers')): ?>
-                            <?php while(have_rows('1130_speakers')): the_row(); ?>
-
-                                <div class="speaker-details">
-                                    <div class="flex">
-                                        <img src="<?php the_sub_field('speaker_image');?>" class="speaker-img">
-                                        <h3><?php the_sub_field('speaker_name_position');?></h3> 
-                                    </div>
-                                        <?php the_sub_field('speaker_info');?>
-                                    
-                                </div>
-
-                            <?php endwhile; ?>
-                        <?php endif; ?>
-                    </div>
+                            </div>
+                        <?php endwhile; ?>
+                    <?php endif; ?>
                 </div>
 
             </div>
--- header.php
+++ header.php
@@ -5,6 +5,16 @@
     <meta name='viewport' content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' />
     <meta name="description" content="Exciting times ahead. One day, you'll discover the vision for the UK business over the year ahead. And your role in helping make it happen. This is that day.">
 
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-77382555-26"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-77382555-26');
+    </script>
+
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
@@ -15,7 +25,7 @@
 
     <title>Just Eat |  WELCOME TO JET SET UK, 2021</title>
     <?php wp_head(); ?>
-    <link rel="stylesheet" href="<?php bloginfo( 'template_directory' ); ?>/sass/css.css?v=2" />
+    <link rel="stylesheet" href="<?php bloginfo( 'template_directory' ); ?>/sass/css.css?v=324" />
     <script>(function(H){H.className=H.className+="js"})(document.documentElement)</script>
   </head>
 <body>
