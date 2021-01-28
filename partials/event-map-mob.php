31fdca4f49f1dd8d57d632c3cda94b2a35edd567
Hayley Duffin <hduffin1@hotmail.com>
2021-01-28 17:36:32 +0000

    add svg icons to event-map-mob.

--- partials/event-map-mob.php
+++ partials/event-map-mob.php
@@ -1,7 +1,36 @@
 <div class="container">
     <div class="icon-wrapper">
+        <img src="<?php bloginfo( 'template_directory' ); ?>/images/game.svg" class="icon" alt="">
+        <a href="<?php the_field('game_link');?>" class="reg-cta">Arcade</a>
+    </div>
 
+    <div class="icon-wrapper">
+        <img src="<?php bloginfo( 'template_directory' ); ?>/images/adapt.svg" class="icon" alt="">
+        <a href="<?php the_field('adapt_inspire_link');?>" class="reg-cta">Adapt & Inspire</a>
+    </div>
 
-        </svg>
+    <div class="icon-wrapper">
+        <img src="<?php bloginfo( 'template_directory' ); ?>/images/restaurant.svg" class="icon" alt="">
+        <a href="<?php the_field('hub_link');?>" class="reg-cta">Restaurent Partners</a>
+    </div>
+
+    <div class="icon-wrapper">
+        <img src="<?php bloginfo( 'template_directory' ); ?>/images/photobooth.svg" class="icon" alt="">
+        <a href="<?php the_field('photobooth_link');?>" class="reg-cta">Photobooth</a>
+    </div>
+
+    <div class="icon-wrapper">
+        <img src="<?php bloginfo( 'template_directory' ); ?>/images/meeting.svg" class="icon" alt="">
+        <a href="<?php the_field('meeting_link');?>" class="reg-cta">Meeting Lounge</a>
+    </div>
+
+    <div class="icon-wrapper">
+        <img src="<?php bloginfo( 'template_directory' ); ?>/images/mosaic.svg" class="icon" alt="">
+        <a href="<?php the_field('mosaic_link');?>" class="reg-cta">Mosaic</a>
+    </div>
+
+    <div class="icon-wrapper">
+        <img src="<?php bloginfo( 'template_directory' ); ?>/images/workshops.svg" class="icon" alt="">
+        <a href="<?php the_field('exhib_workshop_link');?>" class="reg-cta">Workshops</a>
     </div>
 </div>
